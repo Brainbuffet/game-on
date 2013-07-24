87bcc5a9fba4b8425206a0a10be8478420736ea1
nueue <astolfivincent@gmail.com>
2013-07-23 23:51:52 -0700

    Tasks > Metadata > Mastery Message
    *Enhanced Tasks by adding Mastery Message.
    --
    **Planning for front-end development of Mastery Message**

--- types/types.php
+++ types/types.php
@@ -31,12 +31,6 @@ function go_mta_con_meta( array $meta_boxes ) {
 				'options' => go_get_all_ranks()
 			),
 			array(
-				'name' => 'Repeatable',
-				'desc' => ' wether or not the task can be repeated',
-				'id'   => $prefix . 'task_repeat',
-				'type' => 'checkbox'
-			),
-			array(
 				'name' => 'Points',
 				'desc' => 'points awarded for encountering, accepting, completing, and mastering the task. (comma seperated, e.g. 10,20,50,70)',
 				'id'   => $prefix . 'task_points',
@@ -48,8 +42,30 @@ function go_mta_con_meta( array $meta_boxes ) {
 				'id'   => $prefix . 'task_currency',
 				'type' => 'text',
 			),
-		),
-	);
+			array(
+				'name' => 'Mastery Message (Optional)',
+				'desc' => 'Enter a message for the user to recieve when they have completed the task',
+				'id' => $prefix . 'mastery_message',
+				'type' => 'wysiwyg',
+				'options' => array(
+	    			'wpautop' => true, // use wpautop?
+	    			'media_buttons' => true, // show insert/upload button(s)
+	    			'textarea_name' => 'mastery_message', // set the textarea name to something different, square brackets [] can be used here
+	    			'textarea_rows' => get_option('default_post_edit_rows', 10), // rows="..."
+	    			'teeny' => false, // output the minimal editor config used in Press This
+	    			'dfw' => false, // replace the default fullscreen with DFW (needs specific css)
+	    			'tinymce' => true, // load TinyMCE, can be used to pass settings directly to TinyMCE using an array()
+	    			'quicktags' => true // load Quicktags, can be used to pass settings directly to Quicktags using an array()	
+					),
+				),
+			array(
+				'name' => 'Repeatable',
+				'desc' => ' wether or not the task can be repeated',
+				'id'   => $prefix . 'task_repeat',
+				'type' => 'checkbox'
+			),
+			),
+		);
 	// Store Meta Boxes
 	$meta_boxes[] = array(
 		'id'         => 'go_mta_metabox',
