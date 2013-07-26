cf56e931c0a10e31dfd092818d4509b535cca400
nueue <astolfivincent@gmail.com>
2013-07-25 17:51:23 -0700

    Clearing Repo

--- go_message.php
+++
@@ -1,5 +0,0 @@
-<?php
-function go_message($the_message) {
-	echo $the_message;
-}
-?>
--- scripts/go_enque.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-function go_jquery() {
-    wp_enqueue_script( 'jquery' );
-	wp_enqueue_script( 'go_jquery', plugin_dir_url(__FILE__).'go_test_jquery.js');
-}
-
-?>
