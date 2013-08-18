6f1d9ea900c875eae148f5a5e9f7f12d81aa0a2f
Semar Yousif <semarpetrus@yahoo.com>
2013-08-17 18:19:23 -0700

    Updated Options page with new code format
    Updated Options page with new code format and removed confusing box

--- go_options.php
+++ go_options.php
@@ -9,7 +9,7 @@ function go_opt_style() {
 }
 add_action('admin_enqueue_scripts', 'go_opt_style');
 
-function sub_option($explanation_name, $explanation, $title, $field_name, $option_name, $explanation_question){ ?>
+function go_sub_option($explanation_name, $explanation, $title, $field_name, $option_name, $explanation_question){ ?>
 	    <div class="pa">
             	<?php go_opt_help($explanation_name,$explanation); ?> 
             	<strong><?= $title ?>:</strong><br />  
@@ -18,6 +18,7 @@ function sub_option($explanation_name, $explanation, $title, $field_name, $optio
             </div> <?php
             
 	}
+add_action('go_sub_option','go_sub_option');
 function game_on_options() { ?>  
     <div class="go-wrap">  
         <h2>Game On Options</h2>  
@@ -25,53 +26,30 @@ function game_on_options() { ?>
             <?php wp_nonce_field('update-options') ?> 
             <div id="tsk" class="opt-box">
             <h3>Task Settings</h3>
-            <div class="pa">
-            	<?php go_opt_help('tasks_name', 'This is the word that will be used in place of Task all over your website. Make sure that it is singular (e.g. Assignment, Quest)'); ?> 
-            	<strong>Singular Tasks Name:</strong><br />
-                <input type="text" name="go_tasks_name" size="45" value="<?php echo get_option('go_tasks_name'); ?>" /><br />
-                <i>what would you like tasks to be called? (singular)</i> 
-            </div>
-            <div class="pa">
-            	<?php go_opt_help('tasks_plural_name', 'This is the word used in place of Task all over your website. Use only plural words here (e.g. Assignments, Quests)'); ?> 
-            	<strong>Plural Tasks Name:</strong><br />  
-                <input type="text" name="go_tasks_plural_name" size="45" value="<?php echo get_option('go_tasks_plural_name'); ?>" /><br />
-                <i>what would you like tasks to be called? (plural)</i> 
-            </div>
+            	<?php 
+				go_sub_option( 'tasks_name', 'This is the word that will be used in place of Task all over your website. Make sure that it is singular (e.g. Assignment, Quest)', 'Singular Tasks Name', 'go_tasks_name', 'go_tasks_name', 'what would you like tasks to be called? (singular)' );
+				go_sub_option('tasks_plural_name','This is the word used in place of Task all over your website. Use only plural words here (e.g. Assignments, Quests)','Plural Tasks Name', 'go_tasks_plural_name', 'go_tasks_plural_name', 'what would you like tasks to be called? (plural)' );
+
+?>
+        
+           
             </div>
             <br />
             <div id="curr" class="opt-box">
             <h3>Currency Settings</h3>
+            <?= go_sub_option( 'tasks_currency_sym', 'The symbol used to represent your currency, such as a dollar sign.', 'Currency Symbol', 'go_currency_sym', 'go_currency_sym', 'what symbol would you like associated with points?' ); ?>
             
             
+            <?= go_sub_option('currency_name', 'This is what your currency will be called. Use a name like Dollars, or Gold.','Currency Name','go_currency_name', 'go_currency_name', 'what would you like currency to be called?' ); ?>
         
             
             
-            <div class="pa">
-            	<?php go_opt_help('tasks_currency_sym', 'The symbol used to represent your currency, such as a dollar sign.'); ?> 
-            	<strong>Currency Symbol:</strong><br />  
-                <input type="text" name="go_currency_sym" size="45" value="<?php echo get_option('go_currency_sym'); ?>" /><br /> 
-                <i>what symbol would you like associated with points?</i> 
-            </div>
+            
             </div><br />
 
-    
-    
-    
-<div class="opt-box">
-            <h3>Title</h3>
-            <div class="pa">
-            	<?php go_opt_help('field','Explanation'); ?> 
-            	<strong>Currency Name:</strong><br />  
-                <input type="text" name="go_currency_name" size="45" value="<?php echo get_option('go_currency_name'); ?>" /><br />
-                <i>what would you like currency to be called?</i> 
-            </div>
-            <div class="pa">
-            	<?php go_opt_help('tasks_currency_sym', 'The symbol used to represent your currency, such as a dollar sign.'); ?> 
-            	<strong>Currency Symbol:</strong><br />  
-                <input type="text" name="go_currency_sym" size="45" value="<?php echo get_option('go_currency_sym'); ?>" /><br /> 
-                <i>what symbol would you like associated with points?</i> 
-            </div>    
-</div>        
+             
+            
+         
             
             
             
@@ -83,23 +61,16 @@ function game_on_options() { ?>
             <br />
             <div id="poi" class="opt-box">       
             <h3>Points Settings</h3>
-            <div class="pa">
-            	<?php go_opt_help('tasks_points_name', 'This is what your points will be called. Use a name like Points, or Experience.'); ?> 
-            	<strong>Points Name:</strong><br /> 
-                <input type="text" name="go_points_name" size="45" value="<?php echo get_option('go_points_name'); ?>" /><br />
-                <i>what would you like points to be called?</i> 
-            </div>
-            <div class="pa">
-            	<?php go_opt_help('tasks_points_sym', 'This is optional. You can use a symbol for points just like you do for currency.'); ?> 
-                <strong>Points Symbol:</strong><br />  
-                <input type="text" name="go_points_sym" size="45" value="<?php echo get_option('go_points_sym'); ?>" /><br />
-                <i>what symbol would you like associated with points?</i>   
-            </div>
+           <?= go_sub_option('tasks_points_name', 'This is what your points will be called. Use a name like Points, or Experience.', 'Points Name', 'go_points_name', 'go_points_name', 'what would you like points to be called?'); ?>
+           <?= go_sub_option('tasks_points_sym', 'This is optional. You can use a symbol for points just like you do for currency.', 'Points Symbol', 'go_points_sym' , 'go_points_sym', 'what symbol would you like associated with points?' ); ?>
+          
+          
             </div>
             <span class="opt-inp"><input type="submit" name="Submit" value="Save Options" /> </span> 
             <input type="hidden" name="action" value="update" />  
             <input type="hidden" name="page_options" value="go_tasks_name,go_tasks_plural_name,go_currency_name,go_currency_sym,go_points_name,go_points_sym" />  
         </form>
+        <?php /*
         <div id="periods_container">
        		<ul id="sortable_go_periods">
        <?php
@@ -115,11 +86,10 @@ function game_on_options() { ?>
        <button style="width:170px;" onclick="go_periods_new_input();" id="go_periods_add_input">New</button>
        <button style="width:170px;" onclick="go_periods_save();" id="go_periods_add_input">Save</button>
         </div>
-    
-<?php
+   
 go_style_periods();
-go_jquery_periods();
-}
+go_jquery_periods();  */
+} 
 function add_game_on_options() {  
     add_menu_page('Game On', 'Game On', 'manage_options', 'game-on-options.php','game_on_options', plugins_url( 'images/ico.png' , __FILE__ ), '81');  
 }
