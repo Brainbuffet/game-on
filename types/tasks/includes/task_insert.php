01daf403f984a047403f05966fe22b9f91171760
Semar Yousif <semarpetrus@yahoo.com>
2013-08-20 18:43:19 -0700

    Fix Mastery and Completion Message And Add Link
    Fixes the issue where mastery message removes completion message.
    It also adds a button to tasks editor that directs to the page where the
    task is.

--- types/tasks/includes/task_insert.php
+++ types/tasks/includes/task_insert.php
@@ -12,6 +12,25 @@ function go_task_insert_bttn( $arg, $post_id ){
 	return $arg;
 } 
 add_filter( 'get_sample_permalink_html', 'go_task_insert_bttn',5,2 );
+////////////////////////////////////////////////////////////////////////////////
+
+function go_task_insert_link( $arg, $post_id ){
+	global $is_resetable;
+	global $wpdb;
+if( ereg('edit-slug', $arg) ){
+		$is_resetable = true;
+		
+			$go_page_ids = (int)$wpdb->get_var("SELECT `ID` FROM `".$wpdb->prefix.'posts'."` WHERE `post_parent` = 0 and `post_content` like '%go_task id=\'".$_GET['post']."\'%' limit 1");
+			$option = '<a class="button button-small" target="_blank" href="'.get_permalink($go_page_ids) .'">'. get_the_title($go_page_ids).'</a>';
+			$arg .= '<span id="edit-slug button button-small hide-if-no-js">'.$option.'
+		</span> ';
+		}
+		
+return $arg;
+} 
+add_filter( 'get_sample_permalink_html', 'go_task_insert_link',5,2 );
+
+/////////////////////////////////////////////////////////////////////////////////
 } else {
 // Includes
 include('pop-task.php');
